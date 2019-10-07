569009a967dad3fa0259ad188fe79c09b0732feb
Anwesh43 <anweshthecool0@gmail.com>
2019-10-07 08:52:56 +0530

    adding canvas extension method

--- jumpinblockview/src/main/java/com/anwesh/uiprojects/jumpinblockview/JumpinBlockView.kt
+++ jumpinblockview/src/main/java/com/anwesh/uiprojects/jumpinblockview/JumpinBlockView.kt
@@ -23,3 +23,24 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawJumpinBlock(size : Float, h : Float, scale : Float, paint : Paint) {
+    val deg : Double = (Math.PI / 180f) * scale
+    val y : Float = h * Math.sin(deg).toFloat()
+    save()
+    translate(0f, y)
+    drawRect(RectF(-size, -size, size, size), paint)
+    restore()
+}
+
+fun Canvas.drawJBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    save()
+    translate(gap * (i + 1), h - size)
+    drawJumpinBlock(size, h / 2, scale, paint)
+    restore()
+}
