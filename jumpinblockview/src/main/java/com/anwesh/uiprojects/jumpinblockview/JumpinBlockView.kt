d566da3f947c35c6ab50f7d6c19cda313e40834a
Anwesh43 <anweshthecool0@gmail.com>
2019-10-07 08:44:11 +0530

    adding nodes and constants

--- jumpinblockview/src/main/java/com/anwesh/uiprojects/jumpinblockview/JumpinBlockView.kt
+++ jumpinblockview/src/main/java/com/anwesh/uiprojects/jumpinblockview/JumpinBlockView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.jumpinblockview
 /**
  * Created by anweshmishra on 07/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20 
