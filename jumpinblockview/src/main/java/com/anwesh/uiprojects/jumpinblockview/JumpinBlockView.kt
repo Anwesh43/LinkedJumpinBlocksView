3bc1433a54d083c648bc2e87f3a9e526824dc40b
Anwesh43 <anweshthecool0@gmail.com>
2019-10-07 09:18:51 +0530

    adding JBNode class

--- jumpinblockview/src/main/java/com/anwesh/uiprojects/jumpinblockview/JumpinBlockView.kt
+++ jumpinblockview/src/main/java/com/anwesh/uiprojects/jumpinblockview/JumpinBlockView.kt
@@ -109,4 +109,46 @@ class JumpinBlockView(ctx : Context) : View(ctx ) {
             }
         }
     }
+
+    data class JBNode(var i : Int, val state : State = State()) {
+
+        private var next : JBNode? = null
+        private var prev : JBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = JBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JBNode {
+            var curr : JBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
